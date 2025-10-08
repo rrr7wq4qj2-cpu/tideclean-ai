038ba0ab68e9af0bb0fe35188adfea5a1433df37
rrr7wq4qj2-cpu <rrr7wq4qj2@privaterelay.appleid.com>
2025-10-07 17:17:01 -0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpack: (config) => {
+    // Allow imports like "@/styles/globals.css"
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
 };
+
 module.exports = nextConfig;
